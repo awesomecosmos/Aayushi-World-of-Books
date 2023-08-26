3db2c4bdb1f1e91b4fe654fae8f8ef6e5fe222ea
Aayushi Verma <aayushigeekgirl@gmail.com>
2023-08-26 10:05:08 +0530

    plotly chart and table!

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 import numpy as np  
 import pandas as pd  
-# import plotly.express as px  
+import plotly.express as px
+import plotly.graph_objs as go
+import plotly.offline as pyo
 import streamlit as st  
 from utils import *
 
-st.title("Aayushi's World of Books")
+st.title("Aayushi's World of Books :books: :sparkles:")
 
+with st.expander("Expand to see data! :open_book:"):
+    df = read_books('data/simplified_book_list.xlsx')
+    st.dataframe(df)
+    
 df = read_books('data/simplified_book_list.xlsx')
-st.write(df)
+grouped_df = df.groupby(['Year Read']).count().reset_index()
+
+fig = px.bar(grouped_df,
+             x='Year Read',
+             y='Title',
+             title='Total Books Read by Year',
+             labels={
+                 'Year Read':'Year Read',
+                 'Title':'Total Books Read'}
+             )
+
+st.plotly_chart(fig, use_container_width=True)
